5f98a540649a6de52072dd28ed44996f79d15a00
thien-strongtie <thienguyen@strongtie.com>
2026-09-18 10:32:11 +0700

    chore(styles): Restructure styling folder.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+const path = require('path');
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    additionalData: `
+      @use "@/styles/variables" as *;
+      @use "@/styles/functions" as *;
+      @use "@/styles/mixins" as *;
+    `,
+  },
 };
 
 export default nextConfig;
